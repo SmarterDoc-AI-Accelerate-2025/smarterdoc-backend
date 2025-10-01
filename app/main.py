f6bab59590f6dfcd95f4e86ce66faef0845bd999
Hogan <purmonth@gmail.com>
2025-10-01 16:49:58 -0400

    fix: cors

--- app/main.py
+++ app/main.py
@@ -10,7 +10,10 @@ app = FastAPI(title="SmarterDoc Backend", version="0.1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.CORS_ORIGINS,
+    allow_origins=[
+        "http://localhost:3000",
+        "https://smarterdoc-frontend-1094971678787.us-central1.run.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
